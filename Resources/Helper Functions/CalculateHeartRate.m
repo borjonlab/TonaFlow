c8a4154b70d70228a259b09cbd44047814274314
Manash Sahoo <119986611+Msahoo24@users.noreply.github.com>
2023-09-22 16:12:38 +0200

    bug fixes
    Fixed an issue where some data would  result in incompatible sizes
    
    Added example data

--- Resources/Helper Functions/CalculateHeartRate.m
+++ Resources/Helper Functions/CalculateHeartRate.m
@@ -9,5 +9,13 @@ function rate = CalculateHeartRate(dum, dt ,Fs,BlurWinLen, SmoothingFactor)
 
     % Blur.
     rate = conv(dum, gaussFilter, 'same') * Fs * 60;
+    % if size(dt,2) > size(rate,2)
+    %     rate(end+1) = rate(end);
+    % end
+
+    if size(rate,2) < size(dt,2)
+        nd = size(dt,2) - size(rate,2);
+        rate(end:end+nd) = rate(end);
+    end
     rate = csaps(dt, rate, SmoothingFactor, dt);
 end
