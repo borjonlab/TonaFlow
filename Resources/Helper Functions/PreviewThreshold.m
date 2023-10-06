e53d3090cfcf9f071e1ee41237308b2aa8af68e1
Manash Sahoo <119986611+Msahoo24@users.noreply.github.com>
2023-10-06 14:10:19 -0500

    updates
    Added a respiratory extraction procedure and fixed some things.

---
+++ Resources/Helper Functions/PreviewThreshold.m
@@ -0,0 +1,30 @@
+function [thr, Time] = PreviewThreshold(ECGx,ECGy, prc, Fs, Threshwin)
+    % Function to preview the thresholding from the beat detection dialog
+    % window.
+    sesLen=ECGx(end)-ECGx(1);
+    %%segment into 1 second windows
+    % lseg=1;
+    lseg = Threshwin;
+    lSes=size(ECGx,1);
+    Time=zeros(floor(sesLen/lseg),Fs * lseg); 
+    for g = 1:floor(sesLen/lseg)
+        Time(g,:) = 1+lseg*(g-1)*Fs:lseg*g*Fs;
+    end
+    % Step through time segments and find peaks > 95 percentile
+    spks=[];
+    thr = zeros(1,size(ECGx,2));
+    for g=1:size(Time,1)
+        seg=ECGy(Time(g,:));
+        a=prctile(seg,97.5);
+        thr(Time(g,:)) = repelem(a,1,size(Time,2));
+        spks=[spks; [Time(g,find(seg>a))/Fs]'];
+        clear seg
+    end
+    
+    % thr = reshape(thr,1,size(thr,1)*size(thr,2));
+    % if size(thr,2) < size(ECGx,2)
+    %     df = size(ECGx,2) - size(thr,2);
+    %     thr = [thr repelem(thr(end),1,df)];
+    %     % thr(end:df) = thr(end-1);
+    % end
+end
