db1989bcbcaffadbfb91fa12b041daae6dc62aef
Manash Sahoo <119986611+Msahoo24@users.noreply.github.com>
2023-12-06 12:01:41 -0600

    Build

--- buildApp.m
+++ buildApp.m
@@ -1,8 +1,10 @@
 function status = buildApp
     % This function is used to build the app into a standalone OSX app.
     try
-        mcc -o TonaFlow -W 'main:TonaFlow,version=1.0' -T link:exe -d /Users/manashsahoo/Documents/GitHub/TonaFlow1.0/main/for_testing -v /Users/manashsahoo/Documents/GitHub/TonaFlow1.0/main.mlapp 
+        mcc -o TonaFlow -W 'main:TonaFlow,version=1.0' -T link:exe -d ./main/for_testing -v ./main.mlapp 
+        movefile('./main','./build');
     catch ME
         status = 0;
+        disp(ME);
     end
 end
